99e1537ff3769253ba0d41814ddb2421e46b5e17
awanish <awanish@estreet.in>
2025-12-30 10:19:56 +0530

    course  list footer  fixed

--- client/src/pages/student/CoursesList.jsx
+++ client/src/pages/student/CoursesList.jsx
@@ -8,25 +8,25 @@ import Footer from "../../components/student/Footer";
 
 const CoursesList = () => {
   const { navigate, allCourses } = useContext(AppContext);
-  const {input} = useParams()
+  const { input } = useParams()
   const [filteredCourse, setFilteredCourse] = useState([])
-  useEffect(()=>{
-    if(allCourses && allCourses.length > 0){
+  useEffect(() => {
+    if (allCourses && allCourses.length > 0) {
       const tempCourses = allCourses.slice()
 
       input ?
-      setFilteredCourse(
-        tempCourses.filter(
-          item => item.courseTitle.toLowerCase().includes(input.toLowerCase())
+        setFilteredCourse(
+          tempCourses.filter(
+            item => item.courseTitle.toLowerCase().includes(input.toLowerCase())
+          )
         )
-      )
-    : setFilteredCourse(tempCourses)
+        : setFilteredCourse(tempCourses)
 
     }
-  },[allCourses, input])
+  }, [allCourses, input])
   return (
     <>
-      <div className="relative md:px-36 px-8 pt-20 text-left">
+      <div className="relative md:px-36 px-8 pt-20 text-left flex-1">
         <div className="flex md:flex-row flex-col gap-6 items-start justify-between w-full">
           <div>
             <h1 className="text-4xl font-semibold text-gray-800">Course List</h1>
@@ -40,19 +40,19 @@ const CoursesList = () => {
               / <span>Course List</span>
             </p>
           </div>
-          <SearchBar data={input}/>
+          <SearchBar data={input} />
         </div>
         {input && <div className="inline-flex items-center gap-4 px-4 py-2 border mt-8 -mb-8 text-gray-600">
           <p>{input}</p>
-          <img src={assets.cross_icon} alt="" className="cursor-pointer" onClick={()=>navigate('/course-list')
+          <img src={assets.cross_icon} alt="" className="cursor-pointer" onClick={() => navigate('/course-list')
           } />
-          </div>}
+        </div>}
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 my-16 gap-3 px-2 md:p-0">
-          {filteredCourse.map((course, index)=> <CourseCard key={index} course={course} />)}
-          
+          {filteredCourse.map((course, index) => <CourseCard key={index} course={course} />)}
+
         </div>
       </div>
-      
+      <Footer />
     </>
   );
 };
