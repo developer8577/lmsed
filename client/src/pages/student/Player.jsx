10eb6a683266790c04af8e885440b8350738c31b
awanish <awanish@estreet.in>
2025-12-30 10:36:39 +0530

    player footer fix

--- client/src/pages/student/Player.jsx
+++ client/src/pages/student/Player.jsx
@@ -97,7 +97,7 @@ const Player = () => {
   return courseData ? (
 
     <>
-      <div className="p-4 sm:p-10 flex flex-col-reverse md:grid md:grid-cols-2 gap-10 md:px-36">
+      <div className="p-4 sm:p-10 flex flex-col-reverse md:grid md:grid-cols-2 gap-10 md:px-36 flex-1">
         {/* left column */}
         <div className="text-gray-800">
           <h2 className="text-xl font-semibold">Course Structure</h2>
